4834b2e586e4094822b23b9273539335f1fd4b12
Tianyu Han <petertianyuhan@hotmail.com>
2025-10-27 16:13:53 -0400

    clean up

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,6 +3,6 @@ export default {
   description: `Research in machine learning for healthcare at the University of Pennsylvania.`,
   url: `http://localhost:4321`, // No trailing slash! Update to your production domain when ready.
   name: `Han Lab`, // Shown in the footer copyright
-  enableThemeSwitcher: true,
+  enableThemeSwitcher: false,
   showPlug: false, // Hide template plug in footer for production branding
 };
